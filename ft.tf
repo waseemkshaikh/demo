5d2ce9bd0ec8d19791db4705e40c2be6189fe136
waseemkshaikh <waseemkshaikh2000@gmail.com>
2022-11-10 22:51:21 -0800

    Commiting first git code

---
+++ ft.tf
@@ -0,0 +1,21 @@
+# terraform {
+#   required_providers {
+#     github = {
+#       source = "integrations/github"
+#       version = "5.7.0"
+#     }
+#   }
+# }
+
+
+# provider "github" {
+#     token = "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"
+#     owner = ""
+# }
+
+# resource "github_repository" "example" {
+#   name        = "example" 
+#   visibility = "private"
+
+ 
+# } 
